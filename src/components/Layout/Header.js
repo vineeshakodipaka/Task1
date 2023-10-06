e4fd85c3af97ec3d07c7af1f0603c6d8b2bbacac
vineeshakodipaka <vineeshakodipaka11@gmail.com>
2023-10-06 09:08:01 +0530

    changes done

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,24 +7,27 @@ function Header() {
   return (
     <Navbar
       expand="lg"
-      className="navbarcls pt-5 pb-5 pt-lg-3 pb-lg-3 px-4 mx-5  "
+      className="navbarcls pt-5 pb-5 pt-lg-3 pb-lg-3 px-4 mx-5 lg:text-base md:text-3xl "
     >
-      <Navbar.Brand className="d-lg-none fw-bold text-2xl" href="#home">
+      <Navbar.Brand
+        className="d-lg-none fw-bold lg:text-base md:text-3xl"
+        href="#home"
+      >
         <p className="md:text:4xl">Header</p>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav " />
       <Navbar.Collapse id="responsive-navbar-nav ">
-        <Nav className="ms-lg-5 ps-lg-5 fw-bold  px-md-5 gap-lg-4 justify-content-center flex-grow-1 ">
+        <Nav className="ms-lg-5 ps-lg-5 fw-bold lg:text-base md:text-2xl  gap-lg-4 justify-content-center flex-grow-1 ">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">About</Nav.Link>
           <Nav.Link href="#pricing">Schedules</Nav.Link>
           <Nav.Link href="#pricing">Membership</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
-        <Nav className="ms-auto gap-lg-3 ps-lg-5  px-md-5">
+        <Nav className="ms-auto gap-lg-3 ps-lg-5 lg:text-base md:text-2xl ">
           <Nav.Link href="#deets">Offers</Nav.Link>
           <Button
-            className="rounded-5 md:w-40"
+            className="rounded-5 md:w-40" 
             style={{ background: "#F27A44", border: "none" }}
           >
             Course
